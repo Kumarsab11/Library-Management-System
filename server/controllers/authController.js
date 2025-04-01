e4afea2c9529e6e84c8c40a3057fd628a7d84d92
Kumarsab <himanshuofficial748@gmail.com>
2025-04-01 12:08:34 +0530

    auth routes completed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,9 +2,11 @@ import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/errorMiddlewares.js";
 import { User } from "../models/userModel.js";
 import bcrypt from "bcrypt";
-import crtpto from "crypto";
+import crypto from "crypto";
 import { sendVerificationCode } from "../utils/sendVerificationCode.js";
 import { sendToken } from "../utils/sendToken.js";
+import { generateForgotPasswordEmailTemplate } from "../utils/emailTemplates.js";
+import { sendEmail } from "../utils/sendEmail.js";
 
 export const register = catchAsyncErrors(async (req, res, next) => {
   try {
@@ -136,5 +138,116 @@ export const getUser = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     user,
+  });
+});
+
+export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  if (!req.body.email) {
+    return next(new ErrorHandler("Please enter your email", 400));
+  }
+  const user = await User.findOne({
+    email: req.body.email,
+    accountVerified: true,
+  });
+  if (!user) {
+    return next(new ErrorHandler("Invalid email", 400));
+  }
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
+
+  const message = generateForgotPasswordEmailTemplate(resetPasswordUrl);
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Bookvault Library Management System Password Recovery",
+      message,
+    });
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully.`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+export const resetPassword = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.params;
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+  if (!user) {
+    return next(
+      new ErrorHandler("Reset Password Token is invalid or expired", 400)
+    );
+  }
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Password does not match", 400));
+  }
+  if (
+    req.body.password.length < 8 ||
+    req.body.password.length > 16 ||
+    req.body.confirmPassword.length < 8 ||
+    req.body.confirmPassword.length > 16
+  ) {
+    return next(
+      new ErrorHandler("Password must be between 8 and 16 characters", 400)
+    );
+  }
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  user.password = hashedPassword;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  sendToken(user, 200, "Password Reset Success", res);
+});
+
+export const updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user._id).select("+password");
+  const { currentPassword, newPassword, confirmNewPassword } = req.body;
+  if (!currentPassword || !newPassword || !confirmNewPassword) {
+    return next(new ErrorHandler("Please enter all fields", 400));
+  }
+  const isPasswordMatched = await bcrypt.compare(
+    currentPassword,
+    user.password
+  );
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Current password is incorrect", 400));
+  }
+  if (
+    newPassword.length < 8 ||
+    newPassword.length > 16 ||
+    confirmNewPassword.length < 8 ||
+    confirmNewPassword.length > 16
+  ) {
+    return next(
+      new ErrorHandler("Password must be between 8 and 16 characters", 400)
+    );
+  }
+  if (newPassword !== confirmNewPassword) {
+    return next(new ErrorHandler("Password does not match", 400));
+  }
+
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+  user.password = hashedPassword;
+  await user.save();
+  res.status(200).json({
+    success: true,
+    message: "Password updated successfully",
   });
 });
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,9 +1,12 @@
 import express from "express";
 import {
+  forgotPassword,
   getUser,
   login,
   logout,
   register,
+  resetPassword,
+  updatePassword,
   verifyOTP,
 } from "../controllers/authController.js";
 import { isAuthenticated } from "../middlewares/authMiddleware.js";
@@ -15,5 +18,8 @@ router.post("/verify-otp", verifyOTP);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
 router.get("/me", isAuthenticated, getUser);
+router.post("/password/forget", forgotPassword);
+router.put("/password/reset/:token", resetPassword);
+router.put("/password/update",isAuthenticated, updatePassword);
 
 export default router;
